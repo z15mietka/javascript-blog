3bd9c1aed006949a9c9bfc5a8968342bd0897362
Zaneta Mietka <z.mietka@gmail.com>
2021-08-16 12:56:36 +0200

    add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -39,12 +39,24 @@ for(let activeLink of activeLinks){
 }
 
 /* add class 'active' to the clicked link */
-  clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active');
+console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active');
+/* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.posts article.active');
 
-  for(let activeArticle of activeArticles){
+for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
+
+/* get 'href' attribute from the clicked link */
+const articleSelector = clickedElement.getAttribute ('href');
+console.log(articleSelector);
+
+/* find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
+
+/*  add class 'active' to the correct article */
+targetArticle.classList.add('active');
+}
