a75f700076f2237b281c5cfdbd7c51f675fe408a
Zaneta Mietka <z.mietka@gmail.com>
2021-08-17 11:35:22 +0200

    add styling

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@
     'use strict';
     
     const templates = {
-      articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
-      tagArticleLink: Handlebars.compile(document.querySelector('#template-tag-article-link').innerHTML),
-      authorArticleLink: Handlebars.compile(document.querySelector('#template-author-article-link').innerHTML),
-    }
+        articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
+        tagArticleLink: Handlebars.compile(document.querySelector('#template-tag-article-link').innerHTML),
+        authorArticleLink: Handlebars.compile(document.querySelector('#template-author-article-link').innerHTML),
+        tagCloudLink: Handlebars.compile(document.querySelector('#template-tag-cloud-link').innerHTML),
+        authorSidebarLink: Handlebars.compile(document.querySelector('#template-author-sidebar-link').innerHTML)
+      }
     
-    const opt = {
-      articleSelector: '.post',
-      titleSelector: '.post-title',
-      titleListSelector: '.titles',
+      const opt = {
+        articleSelector: '.post',
+        titleSelector: '.post-title',
+        titleListSelector: '.titles',
+        articleTagsSelector: '.post-tags .list',
+        articleAuthorSelector: '.post-author',
+        tagsListSelector: '.tags.list',
+        authorsListSelector: '.authors.list',
     };
     
     
